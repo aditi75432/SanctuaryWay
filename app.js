e72a7bbb5399d28b8b85e57a8d42919ef0b3ee8c
Aditi Mehta <141945188+aditi75432@users.noreply.github.com>
2024-05-25 23:20:39 +0530

    Update app.js
    Deleted Extra Part

--- app.js
+++ app.js
@@ -1,28 +1,4 @@
-// Special Countdown 
-// let daysItem = document.querySelector("#days");
-// let hoursItem = document.querySelector("#hours");
-// let minItem = document.querySelector("#min");
-// let secItem = document.querySelector("#sec");
 
-// let countDown = () => {
-//     let futureDate = new Date("30 July 2021");
-//     let currentDate = new Date();
-//     let myDate = futureDate - currentDate;
-
-//     let days = Math.floor(myDate / 1000 / 60 / 60 / 24);
-//     let hours = Math.floor(myDate / 1000 / 60 / 60) % 24;
-//     let min = Math.floor(myDate / 1000 / 60) % 60;
-//     let sec = Math.floor(myDate / 1000) % 60;
-
-//     daysItem.innerHTML = days;
-//     hoursItem.innerHTML = hours;
-//     minItem.innerHTML = min;
-//     secItem.innerHTML = sec;
-// }
-
-// countDown()
-
-// setInterval(countDown, 1000)
 
 // Scroll Back To Top 
 function scrollTopBack(){
